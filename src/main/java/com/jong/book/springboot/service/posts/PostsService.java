aa232cf4ef75171fedfafa717cc0bd4eb5c358ac
JongGeun <hamburgermax5@gmail.com>
2021-09-12 15:17:06 +0900

    Update  관련 코드 작성

--- src/main/java/com/jong/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jong/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.jong.book.springboot.service.posts;
 
+import com.jong.book.springboot.domain.posts.Posts;
 import com.jong.book.springboot.domain.posts.PostsRepository;
+import com.jong.book.springboot.web.dto.PostsResponseDto;
 import com.jong.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jong.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,25 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    /*
+    update 진행 시 DB에 쿼리를 날리는 부분이 없는데 그 이유는 JPA의 영속성 컨텍스트 떄문이다.
+    영속성 컨텍스트란 엔티티를 영구 저장하는 환경으로 일종의 논리적 개념이며
+    JPA의 핵심은 엔티티가 영속성 컨텍스트에 포함되어 있냐 아니냐이다.
+    트랜잭션 안에서 데이터베이스를 가져오면 영속성컨텍스트가유지된 상태이다
+    이때 해당 데이터 값을 변경하면 트랙잭션이 끝나는시점에서 해당 테이블에 변경분을 반영한다.
+    즉 엔티티 객체의 값만 변경하면 별도로 Update 쿼리를 날릴 필요가 없다 -> 더티 체킹(dirty checking)
+     */
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
