0fbfc0e094761da1b81edfc9c18bc92467e2ba4f
JongGeun <hamburgermax5@gmail.com>
2021-10-12 17:42:59 +0900

    글 등록 기능을 위한 .mustache 파일, index.js 추가

--- src/main/java/com/jong/book/springboot/web/IndexController.java
+++ src/main/java/com/jong/book/springboot/web/IndexController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    // index
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    // 글등록
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
